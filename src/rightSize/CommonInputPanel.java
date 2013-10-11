833e4be7653f8bba630ea8b52b3314f9caae2908
Mark White <mark@markewhite.com>
2013-10-11 13:13:06 -0400

    Pretty well lined up
    Signed-off-by: Mark White <mark@markewhite.com>

--- src/rightSize/CommonInputPanel.java
+++ src/rightSize/CommonInputPanel.java
@@ -45,119 +45,145 @@ public class CommonInputPanel extends JPanel
 	 * Create the panel.
 	 */
 	public CommonInputPanel() {
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{414, 103, 81, 92, 0};
+		gridBagLayout.rowHeights = new int[]{35, 82, 42, 42, 42, 42, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
 		
-		JLabel lblTitleLabel = new JLabel("Assumptions");
-		lblTitleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		JLabel lblTitleLabel = new JLabel("Assumptions:");
 		lblTitleLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 36));
+		GridBagConstraints gbc_lblTitleLabel = new GridBagConstraints();
+		gbc_lblTitleLabel.anchor = GridBagConstraints.WEST;
+		gbc_lblTitleLabel.fill = GridBagConstraints.VERTICAL;
+		gbc_lblTitleLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblTitleLabel.gridx = 0;
+		gbc_lblTitleLabel.gridy = 0;
+		add(lblTitleLabel, gbc_lblTitleLabel);
 		
-		JLabel label = new JLabel("Target Population Size");
-		label.setFont(new Font("Lucida Grande", Font.PLAIN, 24));
+		JLabel lblTarget = new JLabel("Target Population");
+		lblTarget.setFont(new Font("Lucida Grande", Font.PLAIN, 24));
+		GridBagConstraints gbc_lblTarget = new GridBagConstraints();
+		gbc_lblTarget.anchor = GridBagConstraints.WEST;
+		gbc_lblTarget.insets = new Insets(0, 0, 5, 5);
+		gbc_lblTarget.gridx = 0;
+		gbc_lblTarget.gridy = 2;
+		add(lblTarget, gbc_lblTarget);
 		
 		tfPop = new JTextField();
 		tfPop.setText("100");
 		tfPop.setHorizontalAlignment(SwingConstants.TRAILING);
 		tfPop.setFont(new Font("Lucida Grande", Font.PLAIN, 24));
 		tfPop.setColumns(10);
+		GridBagConstraints gbc_tfPop = new GridBagConstraints();
+		gbc_tfPop.anchor = GridBagConstraints.NORTH;
+		gbc_tfPop.fill = GridBagConstraints.HORIZONTAL;
+		gbc_tfPop.insets = new Insets(0, 0, 5, 5);
+		gbc_tfPop.gridx = 2;
+		gbc_tfPop.gridy = 2;
+		add(tfPop, gbc_tfPop);
 		
 		JLabel label_2 = new JLabel("Expected Proportion");
 		label_2.setFont(new Font("Lucida Grande", Font.PLAIN, 24));
+		GridBagConstraints gbc_label_2 = new GridBagConstraints();
+		gbc_label_2.anchor = GridBagConstraints.SOUTH;
+		gbc_label_2.fill = GridBagConstraints.HORIZONTAL;
+		gbc_label_2.insets = new Insets(0, 0, 5, 5);
+		gbc_label_2.gridx = 0;
+		gbc_label_2.gridy = 3;
+		add(label_2, gbc_label_2);
 		
 		tfProportion = new JTextField();
 		tfProportion.setHorizontalAlignment(SwingConstants.TRAILING);
 		tfProportion.setText("50");
 		tfProportion.setFont(new Font("Lucida Grande", Font.PLAIN, 24));
 		tfProportion.setColumns(10);
+		GridBagConstraints gbc_tfProportion = new GridBagConstraints();
+		gbc_tfProportion.anchor = GridBagConstraints.NORTH;
+		gbc_tfProportion.fill = GridBagConstraints.HORIZONTAL;
+		gbc_tfProportion.insets = new Insets(0, 0, 5, 5);
+		gbc_tfProportion.gridx = 2;
+		gbc_tfProportion.gridy = 3;
+		add(tfProportion, gbc_tfProportion);
 		
-		JLabel lblNewLabel_2 = new JLabel("%");
-		lblNewLabel_2.setFont(new Font("Lucida Grande", Font.PLAIN, 24));
+		JLabel label_4 = new JLabel("%");
+		label_4.setFont(new Font("Lucida Grande", Font.PLAIN, 24));
+		GridBagConstraints gbc_label_4 = new GridBagConstraints();
+		gbc_label_4.fill = GridBagConstraints.HORIZONTAL;
+		gbc_label_4.insets = new Insets(0, 0, 5, 0);
+		gbc_label_4.gridx = 3;
+		gbc_label_4.gridy = 3;
+		add(label_4, gbc_label_4);
 		
 		JLabel lblNewLabel_1 = new JLabel("Desired Confidence Interval");
 		lblNewLabel_1.setFont(new Font("Lucida Grande", Font.PLAIN, 24));
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 0;
+		gbc_lblNewLabel_1.gridy = 4;
+		add(lblNewLabel_1, gbc_lblNewLabel_1);
 		
 		tfCI = new JTextField();
 		tfCI.setHorizontalAlignment(SwingConstants.TRAILING);
 		tfCI.setText("5");
 		tfCI.setFont(new Font("Lucida Grande", Font.PLAIN, 24));
 		tfCI.setColumns(10);
+		GridBagConstraints gbc_tfCI = new GridBagConstraints();
+		gbc_tfCI.anchor = GridBagConstraints.NORTH;
+		gbc_tfCI.fill = GridBagConstraints.HORIZONTAL;
+		gbc_tfCI.insets = new Insets(0, 0, 5, 5);
+		gbc_tfCI.gridx = 2;
+		gbc_tfCI.gridy = 4;
+		add(tfCI, gbc_tfCI);
+		
+		JLabel lblNewLabel = new JLabel("\u00B1");
+		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 24));
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 2;
+		gbc_lblNewLabel.gridy = 4;
+		add(lblNewLabel, gbc_lblNewLabel);
 		
 		JLabel label_1 = new JLabel("%");
 		label_1.setFont(new Font("Lucida Grande", Font.PLAIN, 24));
+		GridBagConstraints gbc_label_1 = new GridBagConstraints();
+		gbc_label_1.anchor = GridBagConstraints.WEST;
+		gbc_label_1.insets = new Insets(0, 0, 5, 0);
+		gbc_label_1.gridx = 3;
+		gbc_label_1.gridy = 4;
+		add(label_1, gbc_label_1);
 		
 		JLabel lblDesiredConfidenceCoefficient = new JLabel("Desired Confidence Coefficient");
 		lblDesiredConfidenceCoefficient.setFont(new Font("Lucida Grande", Font.PLAIN, 24));
+		GridBagConstraints gbc_lblDesiredConfidenceCoefficient = new GridBagConstraints();
+		gbc_lblDesiredConfidenceCoefficient.anchor = GridBagConstraints.NORTHWEST;
+		gbc_lblDesiredConfidenceCoefficient.insets = new Insets(0, 0, 0, 5);
+		gbc_lblDesiredConfidenceCoefficient.gridx = 0;
+		gbc_lblDesiredConfidenceCoefficient.gridy = 5;
+		add(lblDesiredConfidenceCoefficient, gbc_lblDesiredConfidenceCoefficient);
 		
 		tfCC = new JTextField();
 		tfCC.setText("95");
 		tfCC.setHorizontalAlignment(SwingConstants.TRAILING);
 		tfCC.setFont(new Font("Lucida Grande", Font.PLAIN, 24));
 		tfCC.setColumns(10);
+		GridBagConstraints gbc_tfCC = new GridBagConstraints();
+		gbc_tfCC.anchor = GridBagConstraints.NORTH;
+		gbc_tfCC.fill = GridBagConstraints.HORIZONTAL;
+		gbc_tfCC.insets = new Insets(0, 0, 0, 5);
+		gbc_tfCC.gridx = 2;
+		gbc_tfCC.gridy = 5;
+		add(tfCC, gbc_tfCC);
 		
-		JLabel label_4 = new JLabel("%");
-		label_4.setFont(new Font("Lucida Grande", Font.PLAIN, 24));
-		GroupLayout groupLayout = new GroupLayout(this);
-		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(236)
-					.addComponent(lblTitleLabel))
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(46)
-					.addComponent(label)
-					.addGap(73)
-					.addComponent(tfPop, GroupLayout.PREFERRED_SIZE, 198, GroupLayout.PREFERRED_SIZE))
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(46)
-					.addComponent(lblDesiredConfidenceCoefficient)
-					.addGap(124)
-					.addComponent(tfCC, GroupLayout.PREFERRED_SIZE, 48, GroupLayout.PREFERRED_SIZE)
-					.addGap(5)
-					.addComponent(label_4, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE))
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(46)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(label_2, GroupLayout.PREFERRED_SIZE, 330, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 330, GroupLayout.PREFERRED_SIZE))
-					.addGap(149)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(5)
-							.addComponent(tfCI, GroupLayout.PREFERRED_SIZE, 48, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(label_1, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(tfProportion, GroupLayout.PREFERRED_SIZE, 54, GroupLayout.PREFERRED_SIZE)
-							.addGap(5)
-							.addComponent(lblNewLabel_2, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE))))
-		);
-		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addComponent(lblTitleLabel, GroupLayout.PREFERRED_SIZE, 34, GroupLayout.PREFERRED_SIZE)
-					.addGap(52)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(label)
-						.addComponent(tfPop, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(5)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(label_2)
-						.addComponent(lblNewLabel_2)
-						.addComponent(tfProportion, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(lblNewLabel_1)
-						.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-							.addComponent(label_1)
-							.addComponent(tfCI, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
-					.addGap(18)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(tfCC, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(12)
-							.addComponent(lblDesiredConfidenceCoefficient))
-						.addComponent(label_4))
-					.addContainerGap())
-		);
-		setLayout(groupLayout);
+		JLabel lblNewLabel_2 = new JLabel("%");
+		lblNewLabel_2.setFont(new Font("Lucida Grande", Font.PLAIN, 24));
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.anchor = GridBagConstraints.NORTHWEST;
+		gbc_lblNewLabel_2.gridx = 3;
+		gbc_lblNewLabel_2.gridy = 5;
+		add(lblNewLabel_2, gbc_lblNewLabel_2);
 
 	}
 }
--- src/rightSize/SRSResultsPanel.java
+++ src/rightSize/SRSResultsPanel.java
@@ -10,6 +10,7 @@ import javax.swing.JTextField;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+import javax.swing.SwingConstants;
 
 public class SRSResultsPanel extends JPanel {
 	private JTextField textField;
@@ -20,73 +21,78 @@ public class SRSResultsPanel extends JPanel {
 	 * Create the panel.
 	 */
 	public SRSResultsPanel() {
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{327, 118, 234, 0};
+		gridBagLayout.rowHeights = new int[]{43, 42, 42, 42, 42, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
 		
-		JLabel lblNewLabel = new JLabel("Results");
+		JLabel lblNewLabel = new JLabel("Results:");
 		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 36));
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.anchor = GridBagConstraints.NORTHWEST;
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 0;
+		add(lblNewLabel, gbc_lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel("Sample Size");
+		JLabel lblNewLabel_1 = new JLabel("Sample Size Needed");
 		lblNewLabel_1.setFont(new Font("Lucida Grande", Font.PLAIN, 24));
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.anchor = GridBagConstraints.NORTHWEST;
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 0;
+		gbc_lblNewLabel_1.gridy = 2;
+		add(lblNewLabel_1, gbc_lblNewLabel_1);
 		
 		textField = new JTextField();
+		textField.setHorizontalAlignment(SwingConstants.RIGHT);
+		textField.setFont(new Font("Lucida Grande", Font.PLAIN, 24));
 		textField.setColumns(10);
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.anchor = GridBagConstraints.NORTHWEST;
+		gbc_textField.insets = new Insets(0, 0, 5, 0);
+		gbc_textField.gridx = 2;
+		gbc_textField.gridy = 2;
+		add(textField, gbc_textField);
 		
-		JLabel label = new JLabel("Sample Size");
-		label.setFont(new Font("Lucida Grande", Font.PLAIN, 24));
+		JLabel lblFinitePopulationCorrection = new JLabel("Finite Population Correction");
+		lblFinitePopulationCorrection.setFont(new Font("Lucida Grande", Font.PLAIN, 24));
+		GridBagConstraints gbc_lblFinitePopulationCorrection = new GridBagConstraints();
+		gbc_lblFinitePopulationCorrection.anchor = GridBagConstraints.NORTHWEST;
+		gbc_lblFinitePopulationCorrection.insets = new Insets(0, 0, 5, 5);
+		gbc_lblFinitePopulationCorrection.gridx = 0;
+		gbc_lblFinitePopulationCorrection.gridy = 3;
+		add(lblFinitePopulationCorrection, gbc_lblFinitePopulationCorrection);
 		
 		textField_1 = new JTextField();
+		textField_1.setFont(new Font("Lucida Grande", Font.PLAIN, 24));
 		textField_1.setColumns(10);
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.anchor = GridBagConstraints.NORTHWEST;
+		gbc_textField_1.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_1.gridx = 2;
+		gbc_textField_1.gridy = 3;
+		add(textField_1, gbc_textField_1);
 		
-		JLabel label_1 = new JLabel("Sample Size");
-		label_1.setFont(new Font("Lucida Grande", Font.PLAIN, 24));
+		JLabel lblSampleSizeWith = new JLabel("Corrected Sample Size");
+		lblSampleSizeWith.setFont(new Font("Lucida Grande", Font.PLAIN, 24));
+		GridBagConstraints gbc_lblSampleSizeWith = new GridBagConstraints();
+		gbc_lblSampleSizeWith.anchor = GridBagConstraints.SOUTHWEST;
+		gbc_lblSampleSizeWith.insets = new Insets(0, 0, 0, 5);
+		gbc_lblSampleSizeWith.gridx = 0;
+		gbc_lblSampleSizeWith.gridy = 4;
+		add(lblSampleSizeWith, gbc_lblSampleSizeWith);
 		
 		textField_2 = new JTextField();
+		textField_2.setFont(new Font("Lucida Grande", Font.PLAIN, 24));
 		textField_2.setColumns(10);
-		GroupLayout groupLayout = new GroupLayout(this);
-		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
-					.addContainerGap(66, Short.MAX_VALUE)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(229)
-							.addComponent(lblNewLabel))
-						.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
-							.addGroup(groupLayout.createSequentialGroup()
-								.addComponent(label_1)
-								.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-								.addComponent(textField_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-							.addGroup(groupLayout.createSequentialGroup()
-								.addComponent(label)
-								.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-								.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-							.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
-								.addComponent(lblNewLabel_1)
-								.addGap(294)
-								.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))))
-					.addGap(58))
-		);
-		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(48)
-					.addComponent(lblNewLabel)
-					.addGap(5)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(textField_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(lblNewLabel_1)
-							.addGap(8)
-							.addComponent(label)
-							.addGap(15)
-							.addComponent(label_1)))
-					.addContainerGap(91, Short.MAX_VALUE))
-		);
-		setLayout(groupLayout);
+		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
+		gbc_textField_2.anchor = GridBagConstraints.NORTHWEST;
+		gbc_textField_2.gridx = 2;
+		gbc_textField_2.gridy = 4;
+		add(textField_2, gbc_textField_2);
 
 	}
 }
